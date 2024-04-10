a064cc8f7776dc6c8330549cfd40d4d254412add
Tyler Landtroop <98730695+tlandtroop@users.noreply.github.com>
2024-04-10 00:42:24 -0400

    finished

--- tracing.js
+++ tracing.js
@@ -2,10 +2,12 @@ const { Resource } = require("@opentelemetry/resources");
 const {
   SemanticResourceAttributes,
 } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
+// const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+
 //Instrumentations
 const {
   ExpressInstrumentation,
@@ -15,15 +17,21 @@ const {
 } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+
 //Exporter
 module.exports = (serviceName) => {
-  const exporter = new ConsoleSpanExporter();
+  const exporter = new JaegerExporter({
+    serviceName,
+  });
+
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
     }),
   });
+
   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
   provider.register();
   registerInstrumentations({
     instrumentations: [
